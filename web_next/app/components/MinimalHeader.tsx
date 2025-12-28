d83c00b6b6957bb0a08c2c69980cec5f435e6786
mouad fekkak <mouadfekkak2002@gmail.com>
2025-12-28 18:08:39 +0100

    app/components/MinimalHeader.tsx v3.1

--- web_next/app/components/MinimalHeader.tsx
+++ web_next/app/components/MinimalHeader.tsx
@@ -12,14 +12,14 @@ export function MinimalHeader({ current }: { current?: "home" | "history" }) {
   return (
     <header className="h-18 sticky top-0 z-10 border-b border-zinc-200 bg-white/80 backdrop-blur">
       <div className="mx-auto flex max-w-4xl items-center justify-between px-4 py-3">
-        <Link href="/" className="text-2xl uppercase font-semibold tracking-tight text-zinc-900">
+        <Link href="/" className="macondo text-3xl uppercase font-semibold tracking-tight text-zinc-900">
           Car Price
         </Link>
 
         <nav className="flex items-center gap-6 text-base">
           <Link
             href="/"
-            className={`group flex flex-col items-center leading-none ${linkTextClass(
+            className={`font-bold group flex flex-col items-center leading-none ${linkTextClass(
               current === "home"
             )}`}
           >
@@ -33,7 +33,7 @@ export function MinimalHeader({ current }: { current?: "home" | "history" }) {
 
           <Link
             href="/history"
-            className={`group flex flex-col items-center leading-none ${linkTextClass(
+            className={`font-bold group flex flex-col items-center leading-none ${linkTextClass(
               current === "history"
             )}`}
           >
